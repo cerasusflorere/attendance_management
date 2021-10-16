ef2f6255e529ddf3f75827cd4bf74d116f305a60
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2021-10-16 23:06:03 +0900

    Update management.php

--- management.php
+++ management.php
@@ -51,22 +51,26 @@
     array_multisort($sort_keys, SORT_ASC, $logs);
 
     $logs = json_encode($logs);
+    
 ?>
 
 <body>
     <!-- 検索期間選択部分 -->
     <div class='duration-select-area' id='duration-area'>
-        <select id='duration'>
+        <select name='duration' id='duration'>
             <option value='all'>全期間</option>
             <option value='week2'>2週間</option>
             <option value='month1'>1ヵ月</option>
+            <option value='year1'>1年</option>
         </select>
     </div>
-
-    <!-- ダウンロードボタン -->
-    <div class='download-button-area'>
-        <button class='download-button'>ダウンロード</button>
-    </div>
+    
+    
+        <!-- ダウンロードボタン -->
+        <div class='download-button-area'>
+            <button class='download-button' onclick="downloadData()">ダウンロード</button>
+        </div>
+    
 
     <!-- ログ表示部分 -->
     <table border="1" id='table'>
@@ -104,14 +108,16 @@
         window.onload = setAllduration;
         let select_duration = document.getElementById('duration');
         let table = document.getElementById('table');
-        select_duration.onchange = changeDuration;
-        let cells = [];
-        let data_number = 0;        
+        select_duration.onchange = changeDuration;  // 表示を変える
+        let cells = [];  // 表示を変えた際に以前の表示を消す
+        let data_number = 0; // 表示数を管理、これをもとに以前の表示を消す
+        let new_logs = []; // 表示するデータ        
 
         const logs = JSON.parse('<?php echo $logs; ?>');
 
         // 期間が変更されたときの動作
         function changeDuration(){
+            new_logs = [];
             for(let i = 0; i < data_number; i++){
                 const cell = document.getElementById('cell-' + i);
                 cell.remove();  // 表示していた来校ログを削除
@@ -129,12 +135,15 @@
                 set2weeks();
             }else if(changeDuration == 'month1'){
                 set1month();
+            }else if(changeDuration == 'year1'){
+                set1year();
             }
         }
 
         // 全期間が選択されたとき
         function setAllduration(){
-            logs.forEach((log) => {
+            new_logs = logs;
+            new_logs.forEach((log) => {
                 // 1行追加
                 var cellsTr = document.createElement('tr');
                 cellsTr.id = 'cell-' + data_number;
@@ -343,8 +352,6 @@
         function set2weeks(){
             var date_2weeks = new Date();
             date_2weeks.setDate(date_2weeks.getDate()-14); // 2週間前の日付を取得
-
-            let new_logs = [];
 
             logs.forEach((log) => {
                 let new_date = new Date(log['date']);
@@ -563,8 +570,6 @@
             var date_1month = new Date()
             date_1month.setMonth(date_1month.getMonth()-1) // 1ヵ月前の日付を取得
 
-            let new_logs = [];
-
             logs.forEach((log) => {
                 let new_date = new Date(log['date']);
                 if(date_1month <= new_date){
@@ -777,6 +782,242 @@
             })
         }
 
+        // 1年前が選択されたとき
+        function set1year(){
+            var date_1year = new Date()
+            date_1year.setFullYear(date_1year.getFullYear()-1) // 1年前の日付を取得
+
+            logs.forEach((log) => {
+                let new_date = new Date(log['date']);
+                if(date_1year <= new_date){
+                    new_logs.push(log);  // 1年前の日付より後だったもので新しい配列を作成
+                }
+            })
+
+            new_logs.forEach((log) => {
+                // 1行追加
+                var cellsTr = document.createElement('tr');
+                cellsTr.id = 'cell-' + data_number;
+                
+                // 名前
+                var nameTd = document.createElement('td');
+                var nameText = document.createTextNode(log['name']);
+
+                // 日付
+                var dateTd = document.createElement('td');
+                var dateText = document.createTextNode(log['date']);
+
+                // 健康チェック
+                var healthTd = document.createElement('td');
+                if(log['health'] == 1){
+                    var healthText = document.createTextNode('○');
+                }
+
+                // 到着時間
+                var arrivalTd = document.createElement('td');
+                var arrivalText = document.createTextNode(log['arrival_time']);
+
+                // 出立時間
+                var departureTd = document.createElement('td');
+                var departureText = document.createTextNode(log['departure_time']);
+
+                // 来校場所チェック
+                // IN401N
+                var IN401Td = document.createElement('td');
+                if(log['IN401N'] == 1){
+                    var IN401Text = document.createTextNode('○');
+                }
+
+                // IN501N
+                var IN501Td = document.createElement('td');
+                if(log['IN501N'] == 1){
+                    var IN501Text = document.createTextNode('○');
+                }
+
+                // IN505N
+                var IN505Td = document.createElement('td');
+                if(log['IN505N'] == 1){
+                    var IN505Text = document.createTextNode('○');
+                }
+
+                // IN418N
+                var IN418Td = document.createElement('td');
+                if(log['IN418N'] == 1){
+                    var IN418Text = document.createTextNode('○');
+                }
+
+                // IN419N
+                var IN419Td = document.createElement('td');
+                if(log['IN419N'] == 1){
+                    var IN419Text = document.createTextNode('○');
+                }
+
+                // IN601N
+                var IN601Td = document.createElement('td');
+                if(log['IN601N'] == 1){
+                    var IN601Text = document.createTextNode('○');
+                }
+
+                // IN603N
+                var IN603Td = document.createElement('td');
+                if(log['IN603N'] == 1){
+                    var IN603Text = document.createTextNode('○');
+                }
+
+                // IN409N
+                var IN409Td = document.createElement('td');
+                if(log['IN409N'] == 1){
+                    var IN409Text = document.createTextNode('○');
+                }
+
+                // IN412N
+                var IN412Td = document.createElement('td');
+                if(log['IN412N'] == 1){
+                    var IN412Text = document.createTextNode('○');
+                }
+
+                // 医心館その他
+                var INotherTd = document.createElement('td');
+                if(log['IN_other'] != null){
+                    var INotherText = document.createTextNode(log['IN_other']);
+                }
+                
+                // 紫苑館
+                var diningTd = document.createElement('td');
+                if(log['dining'] == 1){
+                    var diningText = document.createTextNode('○');
+                }
+
+                // 購買部
+                var purchasingTd = document.createElement('td');
+                if(log['purchasing'] == 1){
+                    var purchasingText = document.createTextNode('○');
+                }
+
+                // 図書館/LC
+                var libraryTd = document.createElement('td');
+                if(log['library'] == 1){
+                    var libraryText = document.createTextNode('○');
+                }
+
+                // その他その他
+                var otherTd = document.createElement('td');
+                if(log['other'] != null){
+                    var otherText = document.createTextNode(log['other']);
+                }
+
+                nameTd.appendChild(nameText);
+                cellsTr.appendChild(nameTd);  // 名前
+
+                dateTd.appendChild(dateText);
+                cellsTr.appendChild(dateTd);  // 日付 
+
+                if(healthText){
+                    healthTd.appendChild(healthText);
+                }                
+                cellsTr.appendChild(healthTd);  // 健康チェック
+
+                arrivalTd.appendChild(arrivalText);
+                cellsTr.appendChild(arrivalTd);  // 到着時間
+
+                departureTd.appendChild(departureText);
+                cellsTr.appendChild(departureTd);  // 出立時間
+
+                if(IN401Text){
+                    IN401Td.appendChild(IN401Text);
+                }                
+                cellsTr.appendChild(IN401Td);  // IN401
+
+                if(IN501Text){
+                    IN501Td.appendChild(IN501Text);
+                }                
+                cellsTr.appendChild(IN501Td);  // IN501
+
+                if(IN505Text){
+                    healthTd.appendChild(IN505Text);
+                }                
+                cellsTr.appendChild(IN505Td);  // IN505
+
+                if(IN418Text){
+                    IN418Td.appendChild(IN418Text);
+                }                
+                cellsTr.appendChild(IN418Td);  // IN418
+
+                if(IN419Text){
+                    IN419Td.appendChild(IN419Text);
+                }                
+                cellsTr.appendChild(IN419Td);  // IN419
+
+                if(IN601Text){
+                    IN601Td.appendChild(IN601Text);
+                }                
+                cellsTr.appendChild(IN601Td);  // IN601
+
+                if(IN603Text){
+                    IN603Td.appendChild(IN603Text);
+                }                
+                cellsTr.appendChild(IN603Td);  // IN603
+
+                if(IN409Text){
+                    IN409Td.appendChild(IN409Text);
+                }                
+                cellsTr.appendChild(IN409Td);  // IN409
+
+                if(IN412Text){
+                    IN412Td.appendChild(IN412Text);
+                }                
+                cellsTr.appendChild(IN412Td);  // IN412
+
+                if(INotherText){
+                    INotherTd.appendChild(INotherText);
+                }                
+                cellsTr.appendChild(INotherTd);  // INその他
+
+                if(diningText){
+                    diningTd.appendChild(diningText);
+                }                
+                cellsTr.appendChild(diningTd);  // 紫苑館
+
+                if(purchasingText){
+                    purchasingTd.appendChild(purchasingText);
+                }                
+                cellsTr.appendChild(purchasingTd);  // 購買部
+
+                if(libraryText){
+                    libraryTd.appendChild(libraryText);
+                }                
+                cellsTr.appendChild(libraryTd);  // 図書館/LC
+
+                if(otherText){
+                    otherTd.appendChild(otherText);
+                }                
+                cellsTr.appendChild(otherTd);  // その他
+
+
+                table.appendChild(cellsTr);
+                data_number++;
+            })
+        }
+
+        // ダウンロードボタンを押された場合
+        function downloadData(){
+            const url = './downloadData.php'; // 通信先
+            const req = new XMLHttpRequest(); // 通信用オブジェクト
+        
+            // 取得した情報をもとにオブジェクトを作る
+            const data = {logs: new_logs};
+        
+            req.onreadystatechange = function() {
+              if(req.readyState == 4 && req.status == 200) {
+                alert("ダウンロードできたと思います");
+              }else if(req.readyState == 4 && req.status != 200) {
+                alert(req.response);
+              }
+            }
+            req.open('POST', url, true);
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.send(JSON.stringify(data)); // オブジェクトを文字列化して送信
+        }
 
     </script>
 </body>
