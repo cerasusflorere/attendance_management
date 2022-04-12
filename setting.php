8766710cd5a0a3a1ab3ef33938b8b31907db76a8
cerasusflorere <86288675+cerasusflorere@users.noreply.github.com>
2022-04-12 21:31:30 +0900

    Update setting.php

--- setting.php
+++ setting.php
@@ -10,12 +10,22 @@
 </head>
 <?php
     session_start();
+
     function h($str)
     {
         return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
     }
+    // envファイル使用のため
+    require '../vendor/autoload.php';
+    // .envを使用する
+    Dotenv\Dotenv::createImmutable(__DIR__)->load();
+    // .envファイルで定義したHOST等を変数に代入
+    $HOST = $_ENV["HOST"];
+    $USER = $_ENV["USER"];
+    $PASS = $_ENV["PASS"];
+    $DB = $_ENV["DB"];    
 
-    $mysqli = new mysqli('***', '***', '***', '***');
+    $mysqli = new mysqli($HOST, $USER, $PASS, $DB);
     if($mysqli->connect_error){
         echo $mysqli->connect_error;
         exit();
